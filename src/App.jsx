648a86e99a595d57102188d7b98f3775ee3bd418
Apurba Dutta <apurbadutta2099@gmail.com>
2025-04-16 15:55:06 +0530

    2. Controlling Elements.  Implement item input functionality in the form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -24,16 +26,38 @@ function Logo() {
 }
 
 function From() {
+  //For input fields
+  const [description, setDescription] = useState("");
+  // Select items
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+    // console.log(e);
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    //Reset the state!
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?🧳</h3>
 
-      <select>
+      {/* set the hooks and change the strings as Number() */}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -41,7 +65,15 @@ function From() {
         ))}
       </select>
 
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
       <button onClick={handleSubmit}>Add</button>
     </form>
   );
